cc1623ccaf9f6a04b82a45db9067b70d198c1395
spandana1998 <31374871+spandana1998@users.noreply.github.com>
2017-08-27 02:00:49 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -7,15 +7,15 @@
 # 2. get the value of the matrix
 # 3. set the value of inverse of the matrix
 # 4. get the value of inverse of the matrix
-makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function(y) {
-    x <<- y
-    inv <<- NULL
+makeCacheMatrix <- function(a = matrix()) {
+  invs <- NULL
+  set <- function(b) {
+    a <<- b
+    invs <<- NULL
   }
-  get <- function() x
-  setinverse <- function(inverse) inv <<- inverse
-  getinverse <- function() inv
+  get <- function() a
+  setinverse <- function(inverse) invs <<- inverse
+  getinverse <- function() invs
   list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
@@ -26,20 +26,20 @@ makeCacheMatrix <- function(x = matrix()) {
 # setinverse function.
 
 # This function assumes that the matrix is always invertible.
-cacheSolve <- function(x, ...) {
-  inv <- x$getinverse()
-  if(!is.null(inv)) {
+cacheSolve <- function(a, ...) {
+  invs <- a$getinverse()
+  if(!is.null(invs)) {
     message("getting cached data.")
-    return(inv)
+    return(invs)
   }
-  data <- x$get()
-  inv <- solve(data)
-  x$setinverse(inv)
-  inv
+  data <- a$get()
+  invs <- solve(data)
+  a$setinverse(invs)
+  invs
 }
 
 ## Sample run:
-## > x = rbind(c(1, -1/4), c(-1/4, 1))
+## > a = rbind(c(1, -1/4), c(-1/4, 1))
 ## > m = makeCacheMatrix(x)
 ## > m$get()
 ##       [,1]  [,2]
